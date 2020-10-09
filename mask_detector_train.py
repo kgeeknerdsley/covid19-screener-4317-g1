6597290c9560a8fa7ec52e5dcef7dbaaaf87cbbc
Kevin Worsley <kgeekmail@gmail.com>
2020-10-09 12:17:17 -0700

    Model can now be trained and saved

--- mask_detector_train.py
+++ mask_detector_train.py
@@ -1,13 +1,22 @@
 import cv2
 import tensorflow as tf
+from tensorflow.keras import layers
 import imutils
 import numpy
+import matplotlib.pyplot as plt
 
 BATCH_SIZE = 32
 IMG_HEIGHT = 150
 IMG_WIDTH = 150
+NUM_CLASSES = 2 #number of classifications, just mask / no mask
 dataDirectory = "/home/kevin/Desktop/AI Project/covidAI/prajna_dataset"
 
+EPOCHS = 10
+LEARNING_RATE = 0.001
+
+print("Loading Dataset")
+
+#set up training dataset
 train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     dataDirectory,
     validation_split=0.2,
@@ -17,7 +26,57 @@ train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
     batch_size = BATCH_SIZE
 )
 
-#program execution
-print("\n\n\n\n\n\n")
-class_names = train_dataset.class_names
-print(class_names)
+#set up validation dataset
+validate_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+  dataDirectory,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(IMG_HEIGHT, IMG_WIDTH),
+  batch_size=BATCH_SIZE)
+
+#cache and optimize data for training
+#caching data stores training data in memory, so we don't have to keep loading and unloading to disk every epoch
+#prefetching allows the execution pipeline to load data for next step, right before training
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
+validate_dataset = validate_dataset.cache().prefetch(buffer_size = AUTOTUNE)
+
+print("Datasets loaded!")
+
+#build the model!
+#NOTE: CURRENTLY, THIS MODEL IS POOP. JUST FOR MAKING SURE TRAIN SCRIPT WORKS
+model = tf.keras.Sequential([
+    layers.experimental.preprocessing.Rescaling(1./255, input_shape = (IMG_HEIGHT,IMG_WIDTH,3)), #rescale image from 3 layer RGB, to 1 layer grayscale
+    layers.Conv2D(64,3, padding='same',activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64,3, padding='same',activation='relu'),
+    layers.Flatten(),
+    layers.Dense(128,activation='relu'),
+    layers.Dense(NUM_CLASSES)
+])
+
+#compile the model
+model.compile(
+    optimizer = 'adam',
+    loss = tf.keras.losses.binary_crossentropy,
+    metrics = ['accuracy']
+)
+
+#check out its structure
+model.summary()
+
+check = input("\nReady to train? Press Y or y when ready\n")
+
+#train the dude!
+model.fit(
+    train_dataset,
+    validation_data= validate_dataset,
+    epochs = EPOCHS
+)
+
+print("\nSaving model...\n")
+model.save('maskface.model')
+print("Model saved!")
+
+
