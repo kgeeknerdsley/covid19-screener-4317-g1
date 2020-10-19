65b42d87f68427cf6b75fe55009269d10f47bca3
Kevin Worsley <kgeekmail@gmail.com>
2020-10-19 13:55:28 -0700

    Returns mask result averaged over five runs

--- mask_detect_camera.py
+++ mask_detect_camera.py
@@ -12,6 +12,9 @@ maskChecks = 0
 maskResult = 0
 checkLimit = 5
 
+hasMask = False
+validResult = False
+
 model_loaded = tf.keras.models.load_model('/home/kevin/Desktop/AI Project/maskface.model')
 
 webcam = cv2.VideoCapture(0)
@@ -48,30 +51,46 @@ while(True):
     imageBox = imageBox[120:360, 210:430]
     cv2.rectangle(imageWithGuide,(210, 120),(430, 360), (0,255,0),thickness=2)
 
+    if(validResult):
+        cv2.putText(imageWithGuide,str(hasMask),(50,500),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+
     cv2.imshow("Live Feed", imageWithGuide)
     cv2.imshow("Calibrated image", calibImage)
 
     if(success):
         if(isNewSubject(imageBox,calibImage)):
+            validResult = True
+            #perform image preprocessing
             imageModified = cv2.cvtColor(imageModified, cv2.COLOR_BGR2GRAY)
             imageModified = imageModified[120:360, 210:430]
             imageModified = cv2.resize(imageModified,(150,150))
             cv2.imshow("Image to model", imageModified)
             imageModified = np.reshape(imageModified,(1,150,150,1))
 
-                #print(np.shape(imageModified))
-
+            #pass to model, get result
             predictions = model_loaded.predict(imageModified) #include file to check out
             result = predictions[0]
 
             #Positive mask ID skews to 0
             # Negative on mask skews to 1
             if(result <= 0.5):
-                print("Mask\n")
-            else:
-                print("No mask\n")
+                #print("Mask\n")
+                maskResult = maskResult + 1
 
-            print("Result: " , str(result))
+            if(maskChecks == checkLimit):
+                maskChecks = 0
+                maskResult = (maskResult / checkLimit)
+
+                if(maskResult >= 0.6):
+                    print("Mask")
+                    hasMask = True
+                else:
+                    print("No mask")
+            else:
+                maskChecks = maskChecks + 1
+        else:
+            validResult = False
+
     else:
         print("Camera did not open")
         break
