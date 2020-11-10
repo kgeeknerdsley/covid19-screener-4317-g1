ebb8604ff944a27bf8509b6a121ea4474cf516c0
Hailey Magana <hailey.mag@gmail.com>
2020-11-09 22:42:28 -0800

    temp scan

--- master-py-script.py
+++ master-py-script.py
@@ -6,6 +6,9 @@ import cv2
 import numpy as np
 import time
 import copy
+from smbus2 import SMBus
+from mlx90614 import MLX90614
+import webbrowser
 
 #imports for symptom voice code
 
@@ -40,10 +43,31 @@ def isNewSubject(currentImage, calibratedImage):
 
 
 #TEMPERATURE CODE HERE
-#
-#
-#
-#
+while (not passedTemp):
+    webbrowser.open(r"/home/pi/Desktop/covid19-screener-4317-g1/temp1.png")
+    ambientTemp1 =  sensor.get_amb_temp()
+    ambientTemp2 =  sensor.get_amb_temp()
+    ambientTemp3 =  sensor.get_amb_temp()
+    avg = (ambientTemp1 + ambientTemp2 + ambientTemp3 ) / 3
+    print (avg)
+    avgF = (avg * 1.8000) + 32
+    print (sensor.get_obj_temp())
+    bus.close()
+
+    if (avgF > 99.0):
+        #Display the image
+        webbrowser.open(r"/home/pi/Desktop/covid19-screener-4317-g1/fail.png")
+        break
+
+    elif (avgF < 94.0):
+         #Display the image
+        webbrowser.open(r"/home/pi/Desktop/covid19-screener-4317-g1/temp2.png")
+        image.show()
+    else:
+        #pass
+        webbrowser.open(r"/home/pi/Desktop/covid19-screener-4317-g1/success.png")
+        passedTemp = True
+        break
 
 #FACE CODE HERE
 secBetween = 3
